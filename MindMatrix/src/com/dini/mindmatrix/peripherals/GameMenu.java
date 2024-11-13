9bd4a8476816b81c2418d6d1d4c777348670479f
DM9978 <kalinisliit@gmail.com>
2024-11-13 08:11:09 +0530

    Version 4

--- MindMatrix/src/com/dini/mindmatrix/peripherals/GameMenu.java
+++ MindMatrix/src/com/dini/mindmatrix/peripherals/GameMenu.java
@@ -2,83 +2,212 @@ package com.dini.mindmatrix.peripherals;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.InputStream;
+import javax.swing.Timer;
 
 public class GameMenu extends JFrame implements ActionListener {
     private static final long serialVersionUID = 1L;
 
     JButton startButton, settingsButton, aboutButton, loginButton, helpButton;
-    JLabel backgroundLabel;
-    public GameMenu() {
-        setSize(690, 500);
-        setLocationRelativeTo(null);
+    JLabel backgroundLabel, logoLabel, profileIconLabel;
+    Font customFont;
+    private Point initialClick;
+    private JButton[] menuButtons;
+    private int currentIndex = -1;
+    private boolean navigationStarted = false;
 
+    public GameMenu() {
+        setSize(690, 520);
+        setLocationRelativeTo(null);
+        setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Welcome to MindMatrix");
 
-        backgroundLabel = new JLabel();
-        backgroundLabel.setLayout(new GridBagLayout());
+        ImageIcon icon = new ImageIcon(getClass().getResource("/resources/icon.png"));
+        setIconImage(icon.getImage());
 
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(10, 0, 10, 0);
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.anchor = GridBagConstraints.CENTER;
+        try {
+            InputStream is = getClass().getResourceAsStream("/resources/rog.otf");
+            customFont = Font.createFont(Font.TRUETYPE_FONT, is).deriveFont(14f);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(customFont);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        startButton = new JButton("Start");
-        settingsButton = new JButton("Settings");
-        aboutButton = new JButton("About");
-        loginButton = new JButton("Login");
-        helpButton = new JButton("Help");
+        ImageIcon backgroundIcon = new ImageIcon(getClass().getResource("/resources/bg.png"));
+        backgroundLabel = new JLabel(backgroundIcon);
+        backgroundLabel.setLayout(null);
 
-        gbc.gridx = 0;
+        ImageIcon logoIcon = new ImageIcon(getClass().getResource("/resources/rr.png"));
+        logoLabel = new JLabel(logoIcon);
+        logoLabel.setBounds((getWidth() - logoIcon.getIconWidth()) / 2, 40, logoIcon.getIconWidth(), logoIcon.getIconHeight());
 
-        gbc.gridy = 0;
-        backgroundLabel.add(startButton, gbc);
+        ImageIcon buttonBackgroundIcon = new ImageIcon(getClass().getResource("/resources/button.png"));
+        startButton = createCustomButton("Start", buttonBackgroundIcon);
+        settingsButton = createCustomButton("Settings", buttonBackgroundIcon);
+        helpButton = createCustomButton("Help", buttonBackgroundIcon);
+        aboutButton = createCustomButton("About", buttonBackgroundIcon);
+        loginButton = createCustomButton("Login", buttonBackgroundIcon);
 
-        gbc.gridy = 1;
-        backgroundLabel.add(loginButton, gbc);
+        startButton.setBounds(240, 125, 200, 50);
+        loginButton.setBounds(240, 185, 200, 50);
+        settingsButton.setBounds(240, 245, 200, 50);
+        helpButton.setBounds(240, 305, 200, 50);
+        aboutButton.setBounds(240, 365, 200, 50);
 
-        gbc.gridy = 2;
-        backgroundLabel.add(settingsButton, gbc);
+        backgroundLabel.add(logoLabel);
+        backgroundLabel.add(startButton);
+        backgroundLabel.add(loginButton);
+        backgroundLabel.add(settingsButton);
+        backgroundLabel.add(helpButton);
+        backgroundLabel.add(aboutButton);
 
-        gbc.gridy = 3;
-        backgroundLabel.add(helpButton, gbc);
-
-        gbc.gridy = 4;
-        backgroundLabel.add(aboutButton, gbc);
-
-
+        setContentPane(backgroundLabel);
 
         startButton.addActionListener(this);
         loginButton.addActionListener(this);
         settingsButton.addActionListener(this);
-        aboutButton.addActionListener(this);
         helpButton.addActionListener(this);
+        aboutButton.addActionListener(this);
 
-        setContentPane(backgroundLabel);
+
+        /**Keyboard navigation method from chatGPT**/
+
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                int keyCode = e.getKeyCode();
+
+                if (keyCode == KeyEvent.VK_DOWN) {
+                    if (!navigationStarted) {
+                        navigationStarted = true;
+                        currentIndex = 0;
+                    } else {
+                        currentIndex = (currentIndex + 1) % menuButtons.length;
+                    }
+                    updateButtonFocus(currentIndex);
+                } else if (keyCode == KeyEvent.VK_UP) {
+                    if (!navigationStarted) {
+                        navigationStarted = true;
+                        currentIndex = 0;
+                    } else {
+                        currentIndex = (currentIndex - 1 + menuButtons.length) % menuButtons.length;
+                    }
+                    updateButtonFocus(currentIndex);
+                } else if (keyCode == KeyEvent.VK_ENTER && navigationStarted) {
+                    menuButtons[currentIndex].doClick();
+                }
+            }
+        });
+
+        setFocusable(true);
         setVisible(true);
+
+        addMouseHoverEffect(startButton);
+        addMouseHoverEffect(loginButton);
+        addMouseHoverEffect(settingsButton);
+        addMouseHoverEffect(helpButton);
+        addMouseHoverEffect(aboutButton);
     }
+
+    private JButton createCustomButton(String text, ImageIcon backgroundIcon) {
+        JButton button = new JButton(text);
+        button.setIcon(backgroundIcon);
+        button.setHorizontalTextPosition(SwingConstants.CENTER);
+        button.setVerticalTextPosition(SwingConstants.CENTER);
+        button.setContentAreaFilled(false);
+        button.setBorderPainted(false);
+        button.setFocusPainted(false);
+        button.setFont(customFont);
+        return button;
+    }
+
+    private void addMouseHoverEffect(JButton button) {
+        button.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                button.setForeground(Color.RED);
+                button.setBorder(BorderFactory.createLineBorder(Color.RED, 2));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                button.setForeground(Color.BLACK);
+                button.setBorder(null);
+            }
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+            }
+        });
+    }
+
+    private void updateButtonFocus(int index) {
+        for (int i = 0; i < menuButtons.length; i++) {
+            JButton button = menuButtons[i];
+            if (i == index) {
+                button.setForeground(Color.RED);
+                button.setBorder(BorderFactory.createLineBorder(Color.RED, 2));
+            } else {
+                button.setForeground(Color.BLACK);
+                button.setBorder(null);
+            }
+        }
+    }
+
+    private boolean startButtonClicked = false;
+    private boolean loginButtonClicked = false;
+    private boolean settingsButtonClicked = false;
+    private boolean helpButtonClicked = false;
+    private boolean aboutButtonClicked = false;
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        resetHoverEffects();
+
         if (e.getSource() == startButton) {
-            GameGUI game = new GameGUI();
-            game.setVisible(true);
-            this.dispose();
+            if (!startButtonClicked) {
+                startButtonClicked = true;
+                GameGUI game = new GameGUI();
+                game.setVisible(true);
+                this.dispose();
+
+            }
+
         } else if (e.getSource() == loginButton) {
-            Login login = new Login (this);
-            login.setVisible(true);
+            if (!loginButtonClicked) {
+                loginButtonClicked = true;
+                Login login = new Login(this);
+            }
+
         } else if (e.getSource() == settingsButton) {
-            JOptionPane.showMessageDialog(this, "Settings menu (Coming soon!)");
+            if (!settingsButtonClicked) {
+                settingsButtonClicked = true;
+            }
+        } else if (e.getSource() == helpButton) {
+            if (!helpButtonClicked) {
+                helpButtonClicked = true;
+                HelpMenu helpMenu = new HelpMenu(this);
+                this.dispose();
+            }
         } else if (e.getSource() == aboutButton) {
-            About about = new About(this);
-            about.setVisible(true);
+            if (!aboutButtonClicked) {
+                aboutButtonClicked = true;
+                About about = new About(this);
+
+            }
         }
-        else if (e.getSource() == helpButton) {
-            HelpMenu help = new HelpMenu(this);
-            help.setVisible(true);
-            this.dispose();
+    }
+
+
+    private void resetHoverEffects() {
+        JButton[] buttons = {startButton, loginButton, settingsButton, helpButton, aboutButton};
+        for (JButton button : buttons) {
+            button.setForeground(Color.BLACK);
+            button.setBorder(null);
         }
     }
 
