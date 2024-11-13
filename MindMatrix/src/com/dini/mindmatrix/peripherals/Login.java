bedbaadcddeb32b7fd96cffbe6de928d03893ab7
DM9978 <kalinisliit@gmail.com>
2024-11-13 17:13:06 +0530

    Version 1

--- MindMatrix/src/com/dini/mindmatrix/peripherals/Login.java
+++ MindMatrix/src/com/dini/mindmatrix/peripherals/Login.java
@@ -3,15 +3,22 @@ package com.dini.mindmatrix.peripherals;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.InputStream;
+import java.sql.*;
 
 public class Login extends JFrame {
 
+    public static boolean isLoggedIn = false;
     private JTextField usernameField;
     private JPasswordField passwordField;
     private JCheckBox showPasswordCheckBox;
     private JButton loginButton;
     private JButton signupButton;
     private JButton backButton;
+    private Font customFontAgency, customFontRog;
+    private JLabel backgroundLabel;
+    public static int loggedInUserId;
+    public static String loggedInUsername;
 
     public Login(JFrame parent) {
         setTitle("Login");
@@ -21,33 +28,83 @@ public class Login extends JFrame {
         setResizable(false);
         setLayout(new BorderLayout());
 
+        ImageIcon icon = new ImageIcon(getClass().getResource("/resources/icon.png"));
+        setIconImage(icon.getImage());
+
+        try {
+            InputStream isRog = getClass().getResourceAsStream("/resources/agency.ttf");
+            customFontAgency = Font.createFont(Font.TRUETYPE_FONT, isRog).deriveFont(Font.BOLD, 20f);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(customFontAgency);
+
+            InputStream isAgency = getClass().getResourceAsStream("/resources/rog.otf");
+            customFontRog = Font.createFont(Font.TRUETYPE_FONT, isAgency).deriveFont(Font.PLAIN, 14f);
+            ge.registerFont(customFontRog);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        ImageIcon backgroundIcon = new ImageIcon(getClass().getResource("/resources/bg.png"));
+        backgroundLabel = new JLabel(backgroundIcon);
+        backgroundLabel.setLayout(new BorderLayout());
+
         JPanel mainPanel = new JPanel(new GridBagLayout());
+        mainPanel.setOpaque(false);
 
         JLabel usernameLabel = new JLabel("Username:");
+        usernameLabel.setFont(customFontAgency);
+        usernameLabel.setForeground(Color.BLACK);
         usernameField = new JTextField(15);
 
         JLabel passwordLabel = new JLabel("Password:");
+        passwordLabel.setFont(customFontAgency);
+        passwordLabel.setForeground(Color.BLACK);
         passwordField = new JPasswordField(15);
 
         showPasswordCheckBox = new JCheckBox("Show Password");
+        showPasswordCheckBox.setForeground(Color.BLACK);
+        showPasswordCheckBox.setOpaque(false);
+        showPasswordCheckBox.setFocusPainted(false);
         showPasswordCheckBox.addActionListener(e -> togglePasswordVisibility());
 
-        loginButton = new JButton("Login");
-        signupButton = new JButton("Signup");
-        backButton = new JButton("Back");
+        loginButton = createCustomButton("Login", new ImageIcon(getClass().getResource("/resources/button4.png")));
+        signupButton = createCustomButton("Signup", new ImageIcon(getClass().getResource("/resources/button4.png")));
+        backButton = createCustomButton("Back", new ImageIcon(getClass().getResource("/resources/button.png")));
 
-        signupButton.addActionListener(e -> new SignUp(Login.this));
-
-        loginButton.addActionListener(e -> {
-
-
-
+        signupButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new SignUp(Login.this);
+            }
         });
 
-        backButton.addActionListener(e -> {
-            dispose();
-            parent.setVisible(true);
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = usernameField.getText();
+                char[] password = passwordField.getPassword();
+                String passwordStr = new String(password);
+
+                if (authenticateUser(username, passwordStr)) {
+                    isLoggedIn = true;
+                    JOptionPane.showMessageDialog(Login.this, "Login Successful!");
+                    dispose();
+                } else {
+                    JOptionPane.showMessageDialog(Login.this, "Invalid username or password.");
+                }
+            }
         });
+
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                parent.setVisible(true);
+            }
+        });
+
+
+        /**Grid layout is from chatGPT**/
 
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(5, 10, 5, 10);
@@ -88,9 +145,11 @@ public class Login extends JFrame {
         gbc.fill = GridBagConstraints.HORIZONTAL;
         mainPanel.add(backButton, gbc);
 
-        add(mainPanel, BorderLayout.CENTER);
+        backgroundLabel.add(mainPanel, BorderLayout.CENTER);
+        setContentPane(backgroundLabel);
         setVisible(true);
     }
+
 
     private void togglePasswordVisibility() {
         if (showPasswordCheckBox.isSelected()) {
@@ -99,4 +158,81 @@ public class Login extends JFrame {
             passwordField.setEchoChar('•');
         }
     }
+
+
+    private boolean authenticateUser(String username, String password) {
+        try {
+            String url = "jdbc:mysql://localhost:3306/mindmatrix";
+            String dbUsername = "root";
+            String dbPassword = "";
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection conn = DriverManager.getConnection(url, dbUsername, dbPassword);
+
+            String query = "SELECT password, player_id FROM users WHERE username = ?";
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, username);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                String storedHashedPassword = rs.getString("password");
+                loggedInUserId = rs.getInt("player_id");
+
+                String hashedInputPassword = SignUp.PasswordUtils.hashPassword(password);
+                if (hashedInputPassword.equals(storedHashedPassword)) {
+                    loggedInUsername = username;
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
+    private JButton createCustomButton(String text, ImageIcon backgroundIcon) {
+        JButton button = new JButton(text);
+        button.setIcon(backgroundIcon);
+        button.setHorizontalTextPosition(SwingConstants.CENTER);
+        button.setVerticalTextPosition(SwingConstants.CENTER);
+        button.setContentAreaFilled(false);
+        button.setBorderPainted(false);
+        button.setFocusPainted(false);
+        button.setFont(customFontRog);
+        addHoverEffect(button);
+        return button;
+    }
+
+    private void addHoverEffect(JButton button) {
+        button.addMouseListener(new java.awt.event.MouseAdapter() {
+            Color originalForeground = button.getForeground();
+
+            @Override
+            public void mouseEntered(java.awt.event.MouseEvent e) {
+                button.setForeground(Color.RED);
+            }
+
+            @Override
+            public void mouseExited(java.awt.event.MouseEvent e) {
+                button.setForeground(originalForeground);
+            }
+        });
+    }
+
+    public int getLoggedInUserId() {
+        return loggedInUserId;
+    }
+
+    public void setLoggedInUserId(int loggedInUserId) {
+        this.loggedInUserId = loggedInUserId;
+    }
+
+    public void setloggedInUsername(String username) {
+        this.usernameField.setText(username);
+    }
 }
+
+
