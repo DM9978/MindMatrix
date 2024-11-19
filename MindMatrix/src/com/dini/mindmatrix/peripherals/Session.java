5b01ae70e9df8dcd3327c7d596d1f8c04d2de65b
DM9978 <kalinisliit@gmail.com>
2024-11-19 16:34:36 +0530

    Initial Commit

---
+++ MindMatrix/src/com/dini/mindmatrix/peripherals/Session.java
@@ -0,0 +1,58 @@
+package com.dini.mindmatrix.peripherals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Session {
+    private static boolean isUserLoggedIn = false;
+    private static int loggedInUserId = -1;
+    private static String loggedInUsername = null;
+    private static Map<String, String> cookies = new HashMap<>();
+
+    public static boolean isUserLoggedIn() {
+        return isUserLoggedIn;
+    }
+
+    public static void setUserLoggedIn(boolean isLoggedIn) {
+        isUserLoggedIn = isLoggedIn;
+    }
+
+    public static int getLoggedInUserId() {
+        return loggedInUserId;
+    }
+
+    public static void setLoggedInUserId(int userId) {
+        loggedInUserId = userId;
+    }
+
+    public static String getLoggedInUsername() {
+        return loggedInUsername;
+    }
+
+    public static void setLoggedInUsername(String username) {
+        loggedInUsername = username;
+    }
+
+    public static void addCookie(String key, String value) {
+        cookies.put(key, value);
+    }
+
+    public static String getCookie(String key) {
+        return cookies.get(key);
+    }
+
+    public static void removeCookie(String key) {
+        cookies.remove(key);
+    }
+
+    public static void clearCookies() {
+        cookies.clear();
+    }
+
+    public static void resetSession() {
+        isUserLoggedIn = false;
+        loggedInUserId = -1;
+        loggedInUsername = null;
+        clearCookies();
+    }
+}
